b1101d5152a16ea5dd0d23ccc68b9206440f950d
Ming Wen <bitmingw@gmail.com>
2015-12-02 17:32:48 -0500

    plot wind data to reveal relationship between wind variables

--- multi_targets_analysis.R
+++ multi_targets_analysis.R
@@ -9,7 +9,7 @@
 # In R console
 
 # Define Global Variables
-MOV_AVG_WINDOW = 21
+MOV_AVG_WINDOW = 30
 
 # Define helper functions
 ma <- function(x, n) {filter(x, rep(1/n, n), sides=2)}
@@ -44,7 +44,7 @@ ma <- function(x, n) {filter(x, rep(1/n, n), sides=2)}
 # 1. Find the time of severe weather by gather differential of major variables
 #    Proposed major variables that indicate severe weather:
 #    TEMPERATURE, VAPOR_PRESSURE, DRY_AIR_PRESSURE, WET_AIR_DENSITY,
-#    WIND_SPEED_AV, RAW_WIND_DIR_AV, RAIN_LAST_HR
+#    WIND_SPEED_AV, RAW_WIND_DIR_AV, RAIN_RATE
 #    Note: need to justify each variable (intuitively)
 #          need to reveal the correlation among a lot of WIND and RAIN variables
 # 2. Find the correlation of weather between stations use the variables above
@@ -72,11 +72,30 @@ data_Pough$Time <- strptime(paste(data_Pough$Sample.Date, data_Pough$Sample.Time
 
 ###############################################################################
 
-# Task0: show the correlation among several wind / rain variables
+# Task0: show the correlation among several rain / wind variables
+
+# RAIN_RATE and RAIN_LAST_HR have exactly the same trend
+# Have no idea what RAIN_TODAY RAINT_THIS_WEEK RAINT_THIS_MONTH etc.
+diff.time <- data_Le$Time
+wind.avg.speed <- data_Le$WIND_SPEED_AV
+wind.sec3.speed <- data_Le$SEC3_ROLL_AVG_WIND_SPEED
+wind.min2.speed <- data_Le$MIN2_ROLL_AVG_WIND_SPEED
+wind.min10.speed <- data_Le$MIN10_ROLL_AVG_WIND_SPEED
+wind.min10.gust.speed <- data_Le$MIN10_WIND_GUST_SPEED
+wind.min60.gust.speed <- data_Le$MIN60_WIND_GUST_SPEED
+wind.speed <- data.frame(diff.time, wind.avg.speed, wind.sec3.speed, wind.min2.speed,
+    wind.min10.speed, wind.min10.gust.speed, wind.min60.gust.speed)
+wind.melt <- melt(wind.speed, id.vars = c("diff.time"))
+wind.plot <- ggplot(wind.melt, aes(x = diff.time, y = value, color = variable)) + geom_line()
+#wind.plot
+# Conclusion: time interval of average is in (3 sec, 2 min)
+# sec3, avg, min2, min10 has exactly the same trend
+# gust10 and gust60 has much higher value than average (indicate max wind speed)
+
+###############################################################################
 
 # Task1: explore the variables using the data from Lexington weather station
 
-diff.time <- data_Le$Time
 diff.temp <- c(0, data_Le$TEMPERATURE[2:nrow(data_Le)] - data_Le$TEMPERATURE[1:nrow(data_Le)-1])
 diff.temp <- diff.temp / max(abs(diff.temp))
 diff.vapor.press <- c(0, data_Le$VAPOR_PRESSURE[2:nrow(data_Le)] - data_Le$VAPOR_PRESSURE[1:nrow(data_Le)-1])
@@ -88,11 +107,12 @@ diff.wet.density <- diff.wet.density / max(abs(diff.wet.density))
 diff.wind.speed <- c(0, data_Le$WIND_SPEED_AV[2:nrow(data_Le)] - data_Le$WIND_SPEED_AV[1:nrow(data_Le)-1])
 diff.wind.speed <- diff.wind.speed / max(abs(diff.wind.speed))
 # diff.wind.dir <- c(0, data_Le$RAW_WIND_DIR_AV[2:nrow(data_Le)] - data_Le$RAW_WIND_DIR_AV[1:nrow(data_Le)-1])
-diff.rain.hr <- c(0, data_Le$RAIN_LAST_HR[2:nrow(data_Le)] - data_Le$RAIN_LAST_HR[1:nrow(data_Le)-1])
-diff.rain.hr <- diff.rain.hr / max(abs(diff.rain.hr))
+diff.rain.rate <- c(0, data_Le$RAIN_RATE[2:nrow(data_Le)] - data_Le$RAIN_RATE[1:nrow(data_Le)-1])
+diff.rain.rate <- diff.rain.rate / max(abs(diff.rain.rate))
 
 # diff.wind.speed temporarily removed since it is not stable...
-diff.data <- data.frame(diff.time, diff.temp, diff.vapor.press, diff.dry.press, diff.wet.density, diff.rain.hr)
+diff.data <- data.frame(diff.time, diff.temp, diff.vapor.press,
+    diff.dry.press, diff.wet.density, diff.rain.rate)
 
 diff.melt <- melt(diff.data, id.vars = c("diff.time"))
 diff.plot <- ggplot(diff.melt, aes(x = diff.time, y = value, color = variable)) + geom_line()
@@ -111,15 +131,17 @@ diff.vapor.press <- abs(c(rep(0, LEADING_ZEROS), diff.vapor.press, rep(0, TRAILI
 diff.dry.press <- abs(c(rep(0, LEADING_ZEROS), diff.dry.press, rep(0, TRAILING_ZEROS)))
 diff.wet.density <- abs(c(rep(0, LEADING_ZEROS), diff.wet.density, rep(0, TRAILING_ZEROS)))
 diff.wind.speed <- abs(c(rep(0, LEADING_ZEROS), diff.wind.speed, rep(0, TRAILING_ZEROS)))
-diff.rain.hr <- abs(c(rep(0, LEADING_ZEROS), diff.rain.hr, rep(0, TRAILING_ZEROS)))
+diff.rain.rate <- abs(c(rep(0, LEADING_ZEROS), diff.rain.rate, rep(0, TRAILING_ZEROS)))
 avg.diff.temp <- as.vector(ma(diff.temp, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
 avg.diff.vapor.press <- as.vector(ma(diff.vapor.press, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
 avg.diff.dry.press <- as.vector(ma(diff.dry.press, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
 avg.diff.wet.density <- as.vector(ma(diff.wet.density, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
 avg.diff.wind.speed <- as.vector(ma(diff.wind.speed, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
-avg.diff.rain.hr <- as.vector(ma(diff.rain.hr, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
+avg.diff.rain.rate <- as.vector(ma(diff.rain.rate, MOV_AVG_WINDOW))[(LEADING_ZEROS+1):(LEADING_ZEROS+nrow(data_Le))]
 
-avg.diff.data <- data.frame(diff.time, avg.diff.temp, avg.diff.vapor.press, avg.diff.dry.press, avg.diff.wet.density, avg.diff.wind.speed, avg.diff.rain.hr)
+# diff.wind.speed temporarily removed since it is not stable...
+avg.diff.data <- data.frame(diff.time, avg.diff.temp, avg.diff.vapor.press,
+    avg.diff.dry.press, avg.diff.wet.density, avg.diff.rain.rate)
 avg.diff.melt <- melt(avg.diff.data, id.vars = c("diff.time"))
 avg.diff.plot <- ggplot(avg.diff.melt, aes(x = diff.time, y = value, color = variable)) + geom_line()
-avg.diff.plot # This plot illustrate the moving average change of values for all those major variables
+#avg.diff.plot # This plot illustrate the moving average change of values for all those major variables
